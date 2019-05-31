5535edadb412e8abf151ccc1b555026dc449cda9
michalkulisiewicz <michalkulisiewicz@gmail.com>
2019-05-31 10:53:25 +0200

    Added try block and finalizing payment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from time import sleep
+from selenium.common.exceptions import NoSuchElementException
 
 
 
@@ -40,15 +41,15 @@ class AllegroAutoBuyer:
 
     def allegro_log_in(self):
         self.browser.get(
-            "https://allegro.pl/login/form?authorization_uri=https:%2F%2Fallegro.pl%2Fauth%2Foauth%2Fauthorize%3Fclient_id%3Dtb5SFf3cRxEyspDN%26redirect_uri%3Dhttps:%2F%2Fallegro.pl%2Flogin%2Fauth%3Forigin_url%253D%25252Flisting%25253Fstring%25253Dlogowanie%26response_type%3Dcode%26state%3Dr4PtdI&oauth=true")
+            "https://allegro.pl/login/form?authorization_uri=https:%2F%2Fallegro.pl%2Fauth%2Foauth%2Fauthorize%3Fclient_id%3Dtb5SFf3cRxEyspDN%26redirect_uri%3Dhttps:%2F%2Fallegro.pl%2Flogin%2Fauth%3Forigin_url%253D%25252F%26response_type%3Dcode%26state%3DpvLd4b&oauth=true")
 
         self.browser.find_element_by_xpath('/html/body/div[4]/div/div[2]/div/div/div/div[2]/div/div[2]/button').click()
 
         self.browser.find_element_by_id("username").clear()
-        self.browser.find_element_by_id("username").send_keys("ugreen")
+        self.browser.find_element_by_id("username").send_keys("lisi3ck4")
         sleep(3)
         self.browser.find_element_by_id("password").clear()
-        self.browser.find_element_by_id("password").send_keys("Alopolska2019#")
+        self.browser.find_element_by_id("password").send_keys("Pieski007#")
         sleep(3)
         self.browser.find_element_by_id("login-button").click()
         sleep(3)
@@ -73,16 +74,26 @@ class AllegroAutoBuyer:
         self.browser.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Dodaj do koszyka'])[1]/following::button[1]").click()
         sleep(3)
-        self.browser.find_element_by_xpath('/html/body/div[1]/div[2]/section/div/section/ui-view/section/form/delivery-section/section/section/ng-form/delivery-bundle/div[3]/section/div/delivery-methods-groups-list/section/ng-form/div[1]/div/div/div[1]/div/div/label/span').click()
-        sleep(3)
-        self.browser.find_element_by_xpath('/html/body/div[1]/div[2]/section/div/section/ui-view/section/form/section[2]/payments-methods/ng-form/div[1]/m-soap-group/div/m-payment-soap[1]/m-soap/div[2]/label/div[2]/div/div').click
-        sleep(3)
-        self.browser.find_element_by_xpath(
-            u"(.//*[normalize-space(text()) and normalize-space(.)='Będziesz mógł sprawdzić wszystkie dane zanim dokonasz zakupu.'])[1]/following::button[1]").click()
+        try:
+            self.browser.find_element_by_xpath('/html/body/div[1]/div[2]/section/div/section/ui-view/section/form/delivery-section/section/section/ng-form/delivery-bundle/div[3]/section/div/delivery-methods-groups-list/section/ng-form/div[1]/div/div/div[1]/div/div/label/span').click()
+        except NoSuchElementException:
+            pass
         sleep(3)
 
+        try:
+            self.browser.find_element_by_xpath('/html/body/div[1]/div[2]/section/div/section/ui-view/section/form/section[2]/payments-methods/ng-form/div[1]/m-soap-group/div/m-payment-soap[1]/m-soap/div[2]/label/div[2]/div/div').click
+        except NoSuchElementException:
+            pass
+        sleep(3)
+        try:
+            self.browser.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Będziesz mógł sprawdzić wszystkie dane zanim dokonasz zakupu.'])[1]/following::button[1]").click()
+        except NoSuchElementException:
+            pass
+        sleep(10)
+        self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/section/div/section/ui-view/section/m-confirm-footer/div/section/ng-transclude[2]/m-buttons-section/m-button-footer/div[2]/div[1]/div/div/div/ng-transclude[3]/m-custom-primary-button/buy-button/button/span[2]/span").click()
+
 auto_buyer = AllegroAutoBuyer()
-auto_buyer.perform('7945990024')
+auto_buyer.perform('8129597339')
 
 # auto_buyer.perform('6570839702')
 
--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@ Based on selenium.
 In order to execute script:
 1. Download geckodriver for firefox from: https://github.com/mozilla/geckodriver/releases
 2. Extract and unzip and move the geckodriver file to /usr/local/bin/ directory if on linux
-or 
+or if on windows add geckodriver to environment variables. as per instruction: https://www.youtube.com/watch?v=KNzGtHI_60o
 3.Use 'pip install -r requiremenets.txt' command in order to install all dependencies.
-
+In order for script to work:
+1.Make first transaction yourself, using card saving details.
