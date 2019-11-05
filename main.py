58c4cf87bcace9aaad2f2ca4a2ddc28d2f6fdc6a
Michal Kulisiewicz <michalkulisiewicz@gmail.com>
2019-11-05 12:27:24 +0100

    fixed selectors

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from selenium.common.exceptions import NoSuchElementException
 class AllegroAutoBuyer:
 
     def __init__(self):
+        self.login = 'lisi3ck4@gmail.com'
+        self.password = 'StarWarsVader2020!'
         # self.website = input('Enter auction number: ')
         # self.user = input('choose account to use [Type 1 for : ugreen]')
         self.options = Options()
@@ -18,7 +20,6 @@ class AllegroAutoBuyer:
         self.browser.set_window_position(0, 0)
         self.accept_next_alert = True
         self.allegro_log_in()
-
 
     def perform(self, auction_number):
         self.search_product_allegro(auction_number)
@@ -47,16 +48,19 @@ class AllegroAutoBuyer:
         self.browser.find_element_by_xpath('/html/body/div[4]/div/div[2]/div/div/div/div[2]/div/div[2]/button').click()
 
         self.browser.find_element_by_id("username").clear()
-        self.browser.find_element_by_id("username").send_keys("lisi3ck4")
+        self.browser.find_element_by_id("username").send_keys(self.login)
         sleep(3)
         self.browser.find_element_by_id("password").clear()
-        self.browser.find_element_by_id("password").send_keys("Pieski007#")
+        self.browser.find_element_by_id("password").send_keys(self.password)
         sleep(3)
         self.browser.find_element_by_id("login-button").click()
         sleep(3)
 
     def search_product_allegro(self, auction_number):
-        self.browser.find_element_by_css_selector("._d25db_31-XG").send_keys(auction_number)
+
+
+        # self.browser.find_element_by_css_selector("._d25db_31-XG").send_keys(auction_number)
+        self.browser.find_element_by_xpath("/html/body/div[2]/div[3]/nav/div/div[1]/div/div/form/input").send_keys(auction_number)
         sleep(3)
         self.browser.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Wszystkie kategorie'])[1]/following::button[1]").click()
@@ -70,16 +74,18 @@ class AllegroAutoBuyer:
         self.browser.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Dodaj do koszyka'])[1]/following::button[1]").click()
         sleep(3)
-
         try:
             self.browser.find_element_by_xpath(
-                '/html/body/div[1]/div[2]/section/div/section/ui-view/section/form/delivery-section/section/section/ng-form/delivery-bundle/div[3]/section/div/delivery-methods-groups-list/section/ng-form/div[1]/div/div/div[1]/div/div/label/span').click()
+                "/html/body/div/div[2]/section/div/section/ui-view/section/section/section/form/delivery-section/section/section/ng-form/delivery-bundle/section[1]/div[2]/delivery-options/delivery-methods-groups/section/ng-form/div[1]/div/div/div[1]/div/div/label").click()
         except NoSuchElementException:
             pass
-
         sleep(5)
-        self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/section/div/section/ui-view/section/m-confirm-footer/div/section/ng-transclude[2]/m-buttons-section/m-button-footer/div[2]/div[1]/div/div/div/ng-transclude[3]/m-custom-primary-button/buy-button/button/span[2]/span").click()
-
+        try:
+            self.browser.find_element_by_xpath("/html/body/div/div[2]/section/div/section/ui-view/section/section/section/form/section[2]/payments-methods/ng-form/div[1]/m-soap-group/div/m-payment-soap[2]/m-soap/div[2]/label/div[2]/div/div").click()
+        except NoSuchElementException:
+            pass
+        sleep(5)
+        self.browser.find_element_by_xpath("/html/body/div/div[2]/section/div/section/ui-view/section/section/aside/div/div/summary-panel/section/section[3]/div/div/buy-button/button/span[2]/span").click()
     def home_page(self):
         self.browser.get('https://allegro.pl/')
 
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ In order to execute script:
 1. Download geckodriver for firefox from: https://github.com/mozilla/geckodriver/releases
 2. Extract and unzip and move the geckodriver file to /usr/local/bin/ directory if on linux
 or if on windows add geckodriver to environment variables. as per instruction: https://www.youtube.com/watch?v=KNzGtHI_60o
-3.Use 'pip install -r requiremenets.txt' command in order to install all dependencies.
+3.Use 'pip install -r requirements.txt' command in order to install all dependencies.
 In order for script to work:
 1.Make first transaction yourself, using card, saving the details.
 2.If reading from file, split each auction number by enter.
